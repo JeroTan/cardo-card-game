b41b1c4e214b61570035054cb0cc219c6ca5eb57
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2025-12-07 22:12:11 +0800

    fix: added cloudflarte adapter

--- src/pages/api/[...slug].ts
+++ src/pages/api/[...slug].ts
@@ -1,4 +1,5 @@
 import { Elysia, t } from 'elysia'
+import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker'
 import { openapi } from '@elysiajs/openapi'
 import getCorsConfig from '@/api/config/cors'
 import { AdminContainer } from '@/container/api/adminContainer';
@@ -6,18 +7,22 @@ import type { APIRoute } from 'astro';
 
 const app = new Elysia({ 
   prefix: '/api',
+  adapter: CloudflareAdapter,
 })
   .use(openapi())
   .use(getCorsConfig())
   // Containers
   .use(AdminContainer(new Elysia()))
+// Required for Cloudflare Workers
 
 // In order to run elysia here
 const handle:APIRoute = (async (ctx) => {
   return await app.decorate({
     env: ctx.locals.runtime.env,
   })
-  .handle(ctx.request);
+  .compile()
+  .handle(ctx.request)
+  ;
 });
 export const GET = handle;
 export const POST = handle;
