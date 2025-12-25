72b22d3e492fec26acd9aaa8991cf5995286f84b
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2025-12-25 15:13:26 +0800

    chore: redesign dashboard

--- src/features/admin/dashboard/DashboardMenu.tsx
+++ src/features/admin/dashboard/DashboardMenu.tsx
@@ -22,21 +22,18 @@ export default function DashboardMenu({ stats = {
         description: "Manage game cards, stats, and artwork",
         icon: CreditCard,
         href: "/admin/cards",
-        color: "bg-blue-500",
       },
       {
         title: "Card Packs",
-        description: "Manage card packs, drop rates, and pricing",
+        description: "Manage card packs",
         icon: Package,
         href: "/admin/card-packs",
-        color: "bg-purple-500",
       },
       {
         title: "User Management",
         description: "View and manage player accounts",
         icon: Users,
         href: "/admin/users",
-        color: "bg-green-500",
       },
     ];
   }, []);
@@ -46,26 +43,18 @@ export default function DashboardMenu({ stats = {
       {
         title: "Total Cards",
         value: stats.totalCards,
-        icon: CreditCard,
-        color: "bg-blue-500",
       },
       {
         title: "Active Packs",
         value: stats.totalPacks,
-        icon: Package,
-        color: "bg-purple-500",
       },
       {
         title: "Total Users",
         value: stats.totalUsers,
-        icon: Users,
-        color: "bg-green-500",
       },
       {
         title: "Active Games",
         value: stats.activeGames ,
-        icon: BarChart3,
-        color: "bg-pink-500",
       },
     ]
   }, [stats]);
@@ -77,15 +66,10 @@ export default function DashboardMenu({ stats = {
       </nav>
 
       {/* Stats Grid */}
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        {statItemsData.slice(0, 4).map((item) => (
-          <Card key={item.title}>
-            <CardContent className="flex gap-5">
-              <div className="flex items-center justify-between">
-                <div className={`rounded-full p-3 ${item.color} bg-opacity-10`}>
-                  <item.icon className={`h-6 w-6 ${item.color.replace('bg-', 'text-')}`} />
-                </div>
-              </div>
+      <div className="flex sm:flex-nowrap flex-wrap justify-content-between gap-4">
+        {statItemsData.map((item) => (
+          <Card key={item.title} className="py-2 px-5 m-0 basis-full">
+            <CardContent className="p-0 flex gap-5">
               <div className="">
                 <p className="text-sm font-medium text-muted-foreground">
                   {item.title}
@@ -100,19 +84,19 @@ export default function DashboardMenu({ stats = {
       </div>
 
       {/* Menu Grid */}
-      <div className="grid gap-4 lg:grid-cols-5 sm:grid-cols-4 ">
+      <div className="grid gap-4 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 ">
         {menuItemData.map((item) => (
           <a
             key={item.title}
             href={item.href}
             className="group"
           >
-            <Card className="relative overflow-hidden transition-all hover:shadow-md hover:border-primary">
+            <Card className="relative h-full overflow-hidden transition-all hover:shadow-md hover:border-primary">
               <CardContent>
                 {/* Icon */}
                 <div className="flex justify-center">
-                  <div className={`relative rounded-full p-3 ${item.color} bg-opacity-10 transition-transform group-hover:scale-110`}>
-                    <item.icon className={`size-8 ${item.color.replace('bg-', 'text-')}`} />
+                  <div className={`relative rounded-full p-3 bg-slate-200 bg-opacity-10 transition-transform group-hover:scale-110`}>
+                    <item.icon color={"gray"} className={`size-8`} />
                   </div>
                 </div>
                 
