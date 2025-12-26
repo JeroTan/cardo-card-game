939d24772c87edb2e6d2fe2631b1c199e9da273a
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2025-12-26 14:48:47 +0800

    chore: ui layout change

--- src/features/admin/card/create/CardCreateForm.tsx
+++ src/features/admin/card/create/CardCreateForm.tsx
@@ -34,128 +34,152 @@ export default function CardCreateForm({
   }, [fieldNameDelay]);
 
   return <>
-    <form>
-      {/* Card Name */}
-      <Field className="gap-1">
-        <FieldLabel htmlFor="card-name">
-          Card Name
-        </FieldLabel>
-        <InputGroup>
-          <InputGroupAddon>
-            <Type />
-          </InputGroupAddon>
-          <InputGroupInput
-            id="card-name"
-            aria-invalid={errorField.name != null ? "true" : "false"}
-            placeholder="Enter name of the card"
-            value={field.name}
-            onInput={(e)=>{
-              updateField({name: (e.target as HTMLInputElement).value});
+    <form className="flex sm:flex-nowrap flex-wrap sm:gap-5">
+      <section className="block sm:basis-2/3 basis-full">
+        {/* Card Name */}
+        <Field className="gap-1">
+          <FieldLabel htmlFor="card-name">
+            Card Name
+          </FieldLabel>
+          <InputGroup>
+            <InputGroupAddon>
+              <Type />
+            </InputGroupAddon>
+            <InputGroupInput
+              id="card-name"
+              aria-invalid={errorField.name != null ? "true" : "false"}
+              placeholder="Enter name of the card"
+              value={field.name}
+              onInput={(e)=>{
+                updateField({name: (e.target as HTMLInputElement).value});
+              }}
+            />
+          </InputGroup>
+          <FieldError className={`${errorField.name == null ? "opacity-0" : "opacity-100"}`}>{errorField.name?.[0] || `None`}</FieldError>
+        </Field>
+        
+        {/* Card Stat */}
+        <Field className="gap-1 mb-7">
+          <FieldLabel htmlFor="card-attack-defense">
+            Attack & Defense
+          </FieldLabel>
+          <InputGroup>
+            <InputGroupAddon>
+              <HandFist />
+            </InputGroupAddon>
+            <InputGroupAddon className="w-full relative cursor-pointer">
+              <Select
+                value={`${field.atk}/${field.def}`}
+                onValueChange={(value)=>{
+                    console.log("Selected Attack/Defense:", value);
+                    const splitValue = value.split("/").map((v)=>parseInt(v, 10));
+                    updateField({atk: splitValue[0], def: splitValue[1]});
+                    updateErrorField({atk: null, def: null});
+                  }}
+              >
+                <SelectTrigger 
+                  className="w-full cursor-pointer"
+                  id="card-attack-defense"
+                >
+                  <SelectValue placeholder="Select Attack & Defense Value" />
+                </SelectTrigger>
+                <SelectContent  id="card-attack-defense" className="w-full">
+                  {Array.from({length: 11}, (_, i) => i).reverse().map((atk, def)=>{
+                    if(def == 0) return null;
+                    const atkTotal = atk%10;
+                    const defTotal = def%10;
+                    const atkDef = `${atkTotal}/${defTotal}`;
+                    return <SelectItem 
+                    className="flex"
+                      key={atkDef}
+                      value={atkDef}
+                    > 
+                      <div className="flex items-center gap-1">
+                        <Sword size={10} />{atkTotal}
+                        <Shield size={10} />{defTotal}
+                      </div>
+                    </SelectItem>
+                  })}
+                </SelectContent>
+              </Select>
+            </InputGroupAddon>
+          </InputGroup>
+        </Field>
+      </section>
+  
+      <section className="sm:basis-1/3 basis-full">
+        {/* Card Art Upload */}
+        <Field className="gap-1">
+          <FieldLabel htmlFor="card_art">
+            Card Art
+          </FieldLabel>
+          <Input
+            id="card_art"
+            className="hidden"
+            type="file"
+            accept="image/*"
+            onChange={(e)=>{
+              const files = (e.target as HTMLInputElement).files;
+              if(files && files.length > 0){
+                updateField({card_art: files[0]});
+                updateErrorField({card_art: null});
+              }
             }}
           />
-        </InputGroup>
-        <FieldError className={`${errorField.name == null ? "opacity-0" : "opacity-100"}`}>{errorField.name?.[0] || `None`}</FieldError>
-      </Field>
-      
-      <Field className="gap-1 mb-7">
-        <FieldLabel htmlFor="card-attack-defense">
-          Attack & Defense
-        </FieldLabel>
-        <InputGroup>
-          <InputGroupAddon>
-            <HandFist />
-          </InputGroupAddon>
-          <InputGroupAddon className="w-full relative cursor-pointer">
-            <Select
-              value={`${field.atk}/${field.def}`}
-              onValueChange={(value)=>{
-                  console.log("Selected Attack/Defense:", value);
-                  const splitValue = value.split("/").map((v)=>parseInt(v, 10));
-                  updateField({atk: splitValue[0], def: splitValue[1]});
-                  updateErrorField({atk: null, def: null});
+          {
+            field.card_art ? <>
+              <div className="flex justify-center gap-4">
+                {/* Image Preview */}
+                <label htmlFor="card_art" className="cursor-pointer relative w-full">
+                  <div className="absolute w-full aspect-[2/3] flex justify-center items-center z-10 bg-slate-500 hover:opacity-50 opacity-0 duration-200">
+                    <ImagePlus size={96} className="text-slate-100"/>
+                  </div>
+                  <div className="w-full aspect-[2/3] border-2 border-gray-300 rounded-lg overflow-hidden bg-gray-50">
+                    <img 
+                      src={URL.createObjectURL(field.card_art)} 
+                      alt="Card preview" 
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                </label>
+              </div>
+            </> : <>
+              <label 
+                htmlFor="card_art" 
+                className="group cursor-pointer border-2 border-dashed border-slate-300 rounded-lg p-8 text-center hover:border-blue-400 transition-colors"
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  e.currentTarget.classList.add('border-blue-500', 'bg-blue-50');
                 }}
-            >
-              <SelectTrigger 
-                className="w-full cursor-pointer"
-                id="card-attack-defense"
+                onDragLeave={(e) => {
+                  e.currentTarget.classList.remove('border-blue-500', 'bg-blue-50');
+                }}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  e.currentTarget.classList.remove('border-blue-500', 'bg-blue-50');
+                  const files = e.dataTransfer.files;
+                  if (files && files.length > 0) {
+                    updateField({card_art: files[0]});
+                    updateErrorField({card_art: null});
+                  }
+                }}
               >
-                <SelectValue placeholder="Select Attack & Defense Value" />
-              </SelectTrigger>
-              <SelectContent  id="card-attack-defense" className="w-full">
-                {Array.from({length: 11}, (_, i) => i).reverse().map((atk, def)=>{
-                  if(def == 0) return null;
-                  const atkTotal = atk%10;
-                  const defTotal = def%10;
-                  const atkDef = `${atkTotal}/${defTotal}`;
-                  return <SelectItem 
-                  className="flex"
-                    key={atkDef}
-                    value={atkDef}
-                  > 
-                    <div className="flex items-center gap-1">
-                      <Sword size={10} />{atkTotal}
-                      <Shield size={10} />{defTotal}
-                    </div>
-                  </SelectItem>
-                })}
-              </SelectContent>
-            </Select>
-          </InputGroupAddon>
-        </InputGroup>
-      </Field>
-
-      {/* Card Art Upload */}
-      <Field className="gap-1">
-        <FieldLabel htmlFor="card_art">
-          Card Art
-        </FieldLabel>
-        <Input
-          id="card_art"
-          className="hidden"
-          type="file"
-          accept="image/*"
-          onChange={(e)=>{
-            const files = (e.target as HTMLInputElement).files;
-            if(files && files.length > 0){
-              updateField({card_art: files[0]});
-              updateErrorField({card_art: null});
-            }
-          }}
-        />
-        {
-          field.card_art ? <>
-            <div className="flex justify-center gap-4">
-              {/* Image Preview */}
-              <label htmlFor="card_art" className="cursor-pointer relative w-full">
-                <div className="absolute w-full aspect-[2/3] flex justify-center items-center z-10 bg-slate-500 hover:opacity-50 opacity-0 duration-200">
-                  <ImagePlus size={96} className="text-slate-100"/>
-                </div>
-                <div className="w-full aspect-[2/3] border-2 border-gray-300 rounded-lg overflow-hidden bg-gray-50">
-                  <img 
-                    src={URL.createObjectURL(field.card_art)} 
-                    alt="Card preview" 
-                    className="w-full h-full object-cover"
-                  />
+                <div className="space-y-2">
+                  <ImagePlus className="mx-auto text-slate-500"/>
+                  <div className="text-sm text-slate-600">
+                    <span className="font-medium text-slate-300 group-hover:text-blue-400 transition-colors">
+                      Click to upload
+                    </span> or drag and drop
+                  </div>
+                  <p className="text-xs text-slate-500">PNG, JPG, GIF up to 10MB</p>
                 </div>
               </label>
-            </div>
-          </> : <>
-            <label htmlFor="card_art" className="cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-8 text-center">
-              <div className="space-y-2">
-                <ImagePlus className="mx-auto text-slate-500"/>
-                <div className="text-sm text-slate-600">
-                  <span className="font-medium text-blue-600 hover:text-blue-500">
-                    Click to upload
-                  </span> or drag and drop
-                </div>
-                <p className="text-xs text-slate-500">PNG, JPG, GIF up to 10MB</p>
-              </div>
-            </label>
-          </>
-        }
-        
-        <FieldError className={`${errorField.card_art == null ? "opacity-0" : "opacity-100"}`}>{errorField.card_art?.join(", ") || `None`}</FieldError>
-      </Field>
+            </>
+          }
+          
+          <FieldError className={`${errorField.card_art == null ? "opacity-0" : "opacity-100"}`}>{errorField.card_art?.join(", ") || `None`}</FieldError>
+        </Field>
+      </section>
     
     </form>
   </>
--- src/features/admin/card/create/CardCreatePage.tsx
+++ src/features/admin/card/create/CardCreatePage.tsx
@@ -1,11 +1,14 @@
-import { useContext, useEffect } from "react";
+import { Fragment, useContext, useEffect } from "react";
 import CardCreateForm from "./CardCreateForm";
 import { CardFieldContext, CardFieldProvider } from "@/stores/card/CardFieldContext";
 import { ErrorFieldContext, ErrorFieldProvider } from "@/stores/card/ErrorFieldContext";
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Minus, Plus } from "lucide-react";
 
 export default function CardCreatePage(){
   return <>
-    <div className="max-w-2xl mx-auto p-6">
+    <div className="max-w-5xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Create New Card</h1>
       <ErrorFieldProvider>
         <CardFieldProvider initialField={1}>
@@ -22,40 +25,69 @@ function CardCreateComposer(){
   const fieldContext = useContext(CardFieldContext);
   const errorFieldContext = useContext(ErrorFieldContext);
 
-  // useEffect(()=>{
-  //   fieldContext.addField();
-  // }, []);
-
   return <>
-    {fieldContext.field.map((fieldItem, index)=>{
-      return <CardCreateForm
-        key={fieldItem.key}
-        field={fieldItem}
-        errorField={{
-          name: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.name`),
-          atk: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.atk`),
-          def: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.def`),
-          card_art: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.card_art`),
-        }}
-        updateField={(updatedField)=>{
-          fieldContext.updateFieldByKey(
-            fieldItem.key,
-            updatedField,
-          );
-        }}
-        updateErrorField={(errorField)=>{
-          console.log("Updating error field for key:", fieldItem.key, errorField);
-          Object.entries(errorField).forEach(([fieldName, messages])=>{
-            if(messages == null || messages.length < 1){
-              errorFieldContext.deleteFieldErrorByKey(`${fieldItem.key}.${fieldName}`);
-              return;
-            }
-            errorFieldContext.setFieldErrorByKey(`${fieldItem.key}.${fieldName}`, messages);
-          })
+    <section className="space-y-4">
+      {fieldContext.field.map((fieldItem, index)=>{
+        return <Fragment key={fieldItem.key}>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              Card #{index+1}
+            </CardTitle>
+            <CardDescription>
+              Create details for card #{index+1}
+            </CardDescription>
+            {fieldContext.field.length > 1 && <CardAction>
+              <Button variant={"ghost"} onClick={()=>{
+                fieldContext.deleteFieldByIndex(index);
+              }}>
+                <Minus /> Remove
+              </Button>
+            </CardAction>}
+          </CardHeader>
+          <CardContent>
+            <CardCreateForm
+              key={fieldItem.key}
+              field={fieldItem}
+              errorField={{
+                name: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.name`),
+                atk: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.atk`),
+                def: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.def`),
+                card_art: errorFieldContext.getFieldErrorQuick(`${fieldItem.key}.card_art`),
+              }}
+              updateField={(updatedField)=>{
+                fieldContext.updateFieldByKey(
+                  fieldItem.key,
+                  updatedField,
+                );
+              }}
+              updateErrorField={(errorField)=>{
+                Object.entries(errorField).forEach(([fieldName, messages])=>{
+                  if(messages == null || messages.length < 1){
+                    errorFieldContext.deleteFieldErrorByKey(`${fieldItem.key}.${fieldName}`);
+                    return;
+                  }
+                  errorFieldContext.setFieldErrorByKey(`${fieldItem.key}.${fieldName}`, messages);
+                });
+              }}
 
-        }}
+            />
+          </CardContent>
+        </Card>
+        </Fragment>
+      }) }
+    </section>
 
-      />
-    }) }
+    <section className="sticky mt-2">
+      <div className="flex justify-center gap-2">
+        <Button variant={"outline"} onClick={()=>fieldContext.addField()}>
+          <Plus /> Card
+        </Button>
+        <Button variant={"default"}>
+          Submit
+        </Button>
+      </div>
+
+    </section>
   </>
 }
