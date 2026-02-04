8495e7f981e8a8d944ed8da45ec612e277b3ae49
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2026-02-04 10:15:32 +0800

    feat: added room id generator for the websocket

---
+++ src/features/game/utils/room.ts
@@ -0,0 +1,11 @@
+export function generateRoomId(length = 8){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let roomId = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    roomId += characters[randomIndex];
+  }
+  
+  return roomId;
+}
